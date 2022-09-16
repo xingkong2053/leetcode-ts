cf832bd4b6b71f61c12297d30ebc59bea26e5c43
wengxiaopeng <3534133221@163.com>
2022-09-16 15:41:54 +0800

    setup environment

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+/** 环境搭建参考 https://juejin.cn/post/6955392566992830477 */
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+};
