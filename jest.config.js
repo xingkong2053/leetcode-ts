7aac2eb22b3b446dab350bb44177fb91d4e0bd08
xingkong2053 <lipenghui2053@gmail.com>
2022-09-24 19:06:00 +0800

    modify jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
+/** 配置参考: https://jestjs.io/zh-Hans/docs/configuration */
 /** 环境搭建参考 https://juejin.cn/post/6955392566992830477 */
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  modulePaths: ['<rootDir>/src/'],
 };
