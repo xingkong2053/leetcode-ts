ac2ce4caa6101970abaa98ff78f2fc2c6bca4892
lipenghui <lipenghui2053@gmail.com>
2022-10-19 10:12:16 +0800

    78. 子集

---
+++ src/78-subsets.ts
@@ -0,0 +1,40 @@
+import { LeftHandSideExpression } from "typescript";
+
+/*
+78. 子集
+给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
+
+解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。
+
+ 
+
+示例 1：
+
+输入：nums = [1,2,3]
+输出：[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+示例 2：
+
+输入：nums = [0]
+输出：[[],[0]]
+ 
+
+提示：
+
+1 <= nums.length <= 10
+-10 <= nums[i] <= 10
+nums 中的所有元素 互不相同
+*/
+export function subsets(nums: number[]): number[][] {
+  // TODO 借助位运算来实现
+  const result: number[][] = [];
+  const len = nums.length;
+  const backtrace = (stack: number[], idx: number) => {
+    result.push([...stack])
+    if(idx === len) return;
+    for(let i = idx + 1; i < len; i++){
+      backtrace([...stack, nums[i]], i);
+    }
+  }
+  backtrace([], -1);
+  return result;
+};
