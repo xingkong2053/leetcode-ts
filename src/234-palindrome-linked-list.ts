68c31caae0956604df1cf6cd643459dadaa6aac5
xingkong2053 <lipenghui2053@gmail.com>
2022-11-20 23:17:29 +0800

    234. 回文链表

---
+++ src/234-palindrome-linked-list.ts
@@ -0,0 +1,42 @@
+/*
+234. 回文链表
+简单
+1.6K
+相关企业
+给你一个单链表的头节点 head ，请你判断该链表是否为回文链表。如果是，返回 true ；否则，返回 false 。
+
+ 
+
+示例 1：
+
+
+输入：head = [1,2,2,1]
+输出：true
+示例 2：
+
+
+输入：head = [1,2]
+输出：false
+ 
+
+提示：
+
+链表中节点数目在范围[1, 105] 内
+0 <= Node.val <= 9
+ 
+
+进阶：你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？
+*/
+import { ListNode } from "data-structure";
+function isPalindrome(head: ListNode | null): boolean {
+  let p: ListNode = head as ListNode;
+  const recursive = (node: ListNode | null) => {
+    if(node){
+      if(!recursive(node.next)) return false;
+      if(node.val !== p.val) return false;
+      p = p.next as ListNode;
+    } 
+    return true;
+  }
+  return recursive(head);
+};
